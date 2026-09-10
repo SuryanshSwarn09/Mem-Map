ae7f05ba66eb32fdfba0e7d94477692e5d35dd37
SURYANSHSwarn09 <suryanshswarn@gmail.com>
2026-09-11 00:02:49 +0530

    Define DuplicateGroup struct with wastedBytes calculation in DuplicateFinder header

--- src/core/DuplicateFinder.h
+++ src/core/DuplicateFinder.h
@@ -10,3 +10,13 @@ struct DuplicateFile {
     int64_t lastModified{0};
     bool isSelectedForDeletion{false};
 };
+
+struct DuplicateGroup {
+    QString hash;
+    int64_t fileSize{0};
+    std::vector<DuplicateFile> files;
+
+    int64_t wastedBytes() const {
+        return files.size() > 1 ? (static_cast<int64_t>(files.size()) - 1) * fileSize : 0;
+    }
+};
