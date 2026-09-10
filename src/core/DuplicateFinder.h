9d30136a80807d9080b282cdb129e56f660b515d
SURYANSHSwarn09 <suryanshswarn@gmail.com>
2026-09-11 00:03:27 +0530

    Declare DuplicateFinder worker class and signal interfaces in DuplicateFinder header

--- src/core/DuplicateFinder.h
+++ src/core/DuplicateFinder.h
@@ -3,6 +3,10 @@
 #include <QString>
 #include <cstdint>
 #include <vector>
+#include <atomic>
+#include <memory>
+#include <QThread>
+#include "DiskNode.h"
 
 struct DuplicateFile {
     QString path;
@@ -33,4 +37,35 @@ struct DuplicateScanResult {
     int64_t totalGroups{0};
     int64_t scannedCandidates{0};
     int64_t elapsedMs{0};
+};
+
+class DuplicateFinder : public QThread {
+    Q_OBJECT
+
+public:
+    explicit DuplicateFinder(QObject* parent = nullptr);
+    ~DuplicateFinder() override;
+
+    void startDuplicateScan(DiskNode* rootNode, HashAlgorithm algo = HashAlgorithm::Md5);
+    void cancelScan();
+    bool isScanning() const { return m_isScanning.load(); }
+
+    static DuplicateScanResult scanDuplicates(DiskNode* rootNode,
+                                              HashAlgorithm algo = HashAlgorithm::Md5,
+                                              std::atomic<bool>* cancelFlag = nullptr);
+
+signals:
+    void scanStarted();
+    void scanProgress(int current, int total, const QString& currentFile);
+    void scanFinished(const DuplicateScanResult& result);
+    void scanCancelled();
+
+protected:
+    void run() override;
+
+private:
+    DiskNode* m_rootNode{nullptr};
+    HashAlgorithm m_algorithm{HashAlgorithm::Md5};
+    std::atomic<bool> m_isScanning{false};
+    std::atomic<bool> m_cancelRequested{false};
 };
